ec1215e1a5252829a25ee2411cb0483a12f2aaac
karthikn-7 <karthikkat7777.com>
2024-08-20 18:20:34 +0530

    update:new methods added - remove,search

--- Tree/Binary_tree.py
+++ Tree/Binary_tree.py
@@ -35,6 +35,44 @@ class BinaryTree:
         if node.right:
             self.display(depth+1,node.right)
 
+    def remove(self,data):
+        if not self.root:
+            print("Binary tree is empty!")
+            return
+        if self.root.data == data:
+            self.root = None
+            return
+        self.removeRecursive(data,self.root)
+        
+        
+    def removeRecursive(self,data,node):
+        if node.left:
+            if node.left.data == data:
+                node.left = None
+                return
+        if node.right:
+            if node.right.data == data:
+                node.right = None
+                return
+        if node.left:
+            self.removeRecursive(data,node.left)
+
+    def search(self,data):
+        if self.root.data == data:
+            return True
+        
+        return self.recursiveSearch(data,self.root)
+        
+        
+    def recursiveSearch(self,data,node):
+        if node and node.data == data:
+            return True
+        if node:
+            return self.recursiveSearch(data,node.left) or self.recursiveSearch(data,node.right)
+        return False
+        
+        
+
 bt = BinaryTree()
 bt.add(10)
 bt.add(20)
@@ -43,5 +81,4 @@ bt.add(40)
 bt.add(50)
 bt.add(60)
 bt.add(70)
-
 bt.display()
